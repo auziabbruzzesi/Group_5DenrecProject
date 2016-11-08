251310f3c8efdfd66ee478493461b60f581df786
Mayah <mayahmcc@udel.edu>
2016-11-08 17:49:35 -0500

    made Wave constants similar to boxes

--- CurrentVersion/src/model/Wave.java
+++ CurrentVersion/src/model/Wave.java
@@ -4,6 +4,12 @@ import java.awt.Point;
 
 public class Wave extends Character {
 	
+		public static final int waveWidth = 50;//wave width 
+		public static final int waveHeight = 50; // wave height 
+		public static final int waveSpawnSpacing = 50; //minimum distance spawned objects should be from waves upon creation 
+		public static final int waveToWaveInterval = 100;//distance between created waves
+		public static final int waveToViewEdgeSpacing = 20;//distance boxes are from the right edge of the screen
+	
 	
 	public Wave(Point p){
 		this.setCurrentPos(p);
@@ -12,10 +18,11 @@ public class Wave extends Character {
 		this.setCurrentPos(p);
 		this.setVelocity(speed);
 	}
-//	@Override
-//	public void move(Point destination) {
-//		// TODO Auto-generated method stub
-//		
-//	}
+	
+		public void move() {
+		// TODO Auto-generated method stub
+	
+	}
+		
 
 }
