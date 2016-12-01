c9227441fa6fc93098e0e17de717ef10a4a43c11
Estella <Estella@myPC>
2016-12-01 14:31:24 -0500

    Played around with resizing View.

--- CurrentVersion/src/controller/Main.java
+++ CurrentVersion/src/controller/Main.java
@@ -1,9 +1,11 @@
 package controller;
 
-
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 //GITHUB SUCKS PLEASE WORK THOUGH SERIOUSLY
 import java.awt.Point;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 //hello
@@ -22,9 +24,17 @@ public class Main {
 	static Controller c = new Controller(m,v);
 	
 	
-
 	public static void main(String[] args) {
 		v.setLayout(null);
+		/*
+		 * Note: I got distracted and started playing around with window resizing. I commented out my changes (below) 
+		 * but left them in here because they might be helpful. If not, just delete them :)
+		 */
+//		v.setLayout(new GridBagLayout());
+//		v.setPreferredSize(new Dimension(View.viewWidth, View.viewHeight));
+//		v.setMinimumSize(new Dimension(700, 150));
+//		v.setMaximumSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize().width , Toolkit.getDefaultToolkit().getScreenSize().height));
+//		v.setResizable(true);
 		v.getJPanel().addMouseListener(c);
 		c.wTimer.start();
 		v.repaint();
