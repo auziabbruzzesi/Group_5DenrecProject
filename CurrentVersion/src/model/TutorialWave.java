a1fd503d63b3859b23d7ba3321a91064ebf2e174
auziabbruzzesi <austinta@udel.edu>
2016-12-05 19:12:41 -0500

    TutorialWave

---
+++ CurrentVersion/src/model/TutorialWave.java
@@ -0,0 +1,13 @@
+package model;
+
+
+import java.awt.Point;
+
+import javax.swing.Icon;
+
+public class TutorialWave extends Wave {
+	
+	public TutorialWave(Point p, Icon k, int shorelineDestX) {
+		super(p, k, shorelineDestX);
+	}
+}
